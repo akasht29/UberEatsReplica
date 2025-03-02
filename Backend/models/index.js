aaa6c0ad0b566562dcc7895330737e9f29615e73
akasht29 <akasht9000@gmail.com>
2025-03-01 22:54:26 -0800

    fixed config and .env

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,20 +1,27 @@
 'use strict';
-
+require('dotenv').config({ path: '../.env' });  // Adjust path as necessary
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+
+
+  const sequelize = new Sequelize(
+    process.env.DB_DATABASE,  // Using DB variables from .env
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, 
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql'  // Or 'postgres', 'sqlite', depending on your DB
+    }
+  );
+
 
 fs
   .readdirSync(__dirname)
