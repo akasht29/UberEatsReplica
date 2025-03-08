7dcbf541a25c9ba929a9446f033e332995768be3
kevincakey <57571360+kevincakey@users.noreply.github.com>
2025-03-07 16:17:55 -0800

    home functionality added

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const BlankPage = () => {
   return (
     <div className="container mt-5">
-      <h1 className="text-center">Welcome to Bootstrap in React</h1>
-
-      <div className="d-flex justify-content-center mt-4">
-        <button className="btn btn-primary">Click Me</button>
-      </div>
+      <Navbar></Navbar>
+      <h1 className="text-center">Order deliver near you</h1>
     </div>
   );
 };
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a
+            class="btn btn-primary"
+            data-bs-toggle="offcanvas"
+            href="#offcanvasExample"
+            role="button"
+            aria-controls="offcanvasExample"
+          >
+            Link with href
+            <span className="navbar-toggler-icon"></span>
+          </a>
+          <button
+            class="btn btn-primary"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasExample"
+            aria-controls="offcanvasExample"
+          >
+            Button with data-bs-target
+          </button>
+          <Link to="/">Home</Link>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarContent"
+          >
+            <div className="d-flex gap-2">
+              <Link to="/customerlogin">login</Link>
+              <Link to="/customersignup">Signup</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasExampleLabel">
+            Offcanvas
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <div>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+          </div>{" "}
+        </div>{" "}
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
